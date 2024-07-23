d1bdbf045011395cf3df922039fba0dc8a56276e
IsmaGold67 <ismaelroberto67@gmail.com>
2024-07-22 22:38:56 -0400

    bug fixed

--- i_convert.py
+++ i_convert.py
@@ -2,6 +2,15 @@ import streamlit as st
 from PIL import Image
 import io
 
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+"""
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 # Mapeamento dos formatos para serem usados pelo Pillow
 format_mapping = {
     "JPEG": "JPEG",
